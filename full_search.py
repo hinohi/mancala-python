fd56c3a4ed05d9c60a182db5ed55ba97b9b810cc
Daiju Nakayama <42.daiju@gmail.com>
2018-07-31 12:05:54 +0900

    DFSで勝/負だけみてreturnするバージョン作成

--- full_search.py
+++ full_search.py
@@ -3,8 +3,12 @@ import json
 import argparse
 from typing import List
 from pathlib import Path
+from collections import namedtuple
 
 from game import MancalaGame, Result
+
+
+SearchResult = namedtuple('SearchResult', ['result', 'record', 'score'])
 
 
 def _dfs(g: MancalaGame, record: List[List[int]]) -> (Result, int, List[List[int]]):
@@ -44,21 +48,60 @@ def _dfs(g: MancalaGame, record: List[List[int]]) -> (Result, int, List[List[int
     return best
 
 
-def dfs(pit, stone):
+def dfs(pit, stone) -> SearchResult:
     g = MancalaGame(pit, stone)
-    return _dfs(g, [])
+    result, score, record = _dfs(g, [])
+    return SearchResult(result, record, score)
+
+
+def _dfs_result_only(g: MancalaGame, record: List[List[int]]) -> (Result, List[List[int]]):
+    if g.state is not Result.IN_BATTLE:
+        return g.state, record
+    win = Result.WIN_PRE if g.side == 0 else Result.WIN_EPI
+    lose = Result.WIN_EPI if g.side == 0 else Result.WIN_PRE
+    best = None
+    for pos in g.pos_candidates():
+        gg = g.copy()
+        gg.move(pos)
+        res = _dfs_result_only(gg, record + [[g.side, pos]])
+        if best is None:
+            best = res
+        if res[0] is win:
+            best = res
+            break
+        elif res[0] is Result.DRAW:
+            if best[0] is lose:
+                best = res
+            elif len(best[1]) < len(res[1]):
+                # 引き分けならばできるだけ試合を引っ張る
+                best = res
+        elif best[0] is lose:
+            if len(best[1]) < len(res[1]):
+                # できるだけ試合を引っ張る
+                best = res
+    return best
+
+
+def dfs_result_only(pit, stone) -> SearchResult:
+    g = MancalaGame(pit, stone)
+    result, record = _dfs_result_only(g, [])
+    return SearchResult(result, record, None)
 
 
 def main():
+    tasks = {
+        'dfs': dfs,
+        'dfs_result_only': dfs_result_only,
+    }
     p = argparse.ArgumentParser()
-    p.add_argument('task', choices=['dfs'])
+    p.add_argument('task', choices=list(tasks))
     p.add_argument('--pit', '-p', type=int, default=4)
     p.add_argument('--stone', '-s', type=int, default=4)
     p.add_argument('--no-debug', action='store_true')
     p.add_argument('--output', '-o')
     args = p.parse_args()
 
-    result, score, record = dfs(args.pit, args.stone)
+    result = tasks[args.task](args.pit, args.stone)
     g = MancalaGame(args.pit, args.stone)
     data = {
         'pit': args.pit,
@@ -69,7 +112,7 @@ def main():
     print(f'stone={args.stone}')
     if not args.no_debug:
         print(g.purity_dumps())
-    for side, pos in record:
+    for side, pos in result.record:
         g.move(pos)
         if not args.no_debug:
             print()
@@ -77,8 +120,8 @@ def main():
             print(f'pos={pos}')
             print(g.purity_dumps())
         data['board'].append(g.copy().board)
-    print(result)
-    print(f'score={score}')
+    print(result.result)
+    print(f'score={result.score}')
 
     if args.output:
         path = Path(args.output)
